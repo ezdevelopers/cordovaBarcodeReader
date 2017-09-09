172235d4ce101bda8564b45b57a04923c315728d
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-09-09 16:38:02 +0100

    Add new plugin for barcode scan

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,9 +1,9 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "id": "cordova-plugin-barcodescanner.BarcodeScanner",
-        "file": "plugins/cordova-plugin-barcodescanner/www/barcodescanner.js",
-        "pluginId": "cordova-plugin-barcodescanner",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "pluginId": "phonegap-plugin-barcodescanner",
         "clobbers": [
             "cordova.plugins.barcodeScanner"
         ]
@@ -13,8 +13,8 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-compat": "1.0.0",
-    "cordova-plugin-barcodescanner": "0.7.2"
+    "cordova-plugin-compat": "1.1.0",
+    "phonegap-plugin-barcodescanner": "6.0.8"
 };
 // BOTTOM OF METADATA
 });

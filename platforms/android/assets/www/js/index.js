065d113dbd890335eeb2c620f793fb69e15feaac
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-09-15 14:50:23 +0100

    Add phone contact options

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,24 +1,38 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+function barcodescanner() {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if (result.text == 9780684852867) {
+                localStorage.setItem('img', 'http://covers.openlibrary.org/b/isbn/9780684852867-L.jpg');
+                localStorage.setItem('Title', 'FIRST, BREAK ALL THE RULES');
+                location.href = "result.html";
+            } else {
+
+                alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }, {
+            preferFrontCamera: false, // iOS and Android
+            showFlipCameraButton: false, // iOS and Android
+            showTorchButton: true, // iOS and Android
+            torchOn: false, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt: "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            //orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations: true, // iOS
+            disableSuccessBeep: false // iOS
+        }
+    );
+}
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -26,12 +40,12 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -40,7 +54,29 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        listContacts();
     }
 };
+
+function listContacts() {
+    var options = new ContactFindOptions();
+    options.filter = "";
+    options.multiple = true;
+    var fields = ["*"];
+    navigator.contacts.find(fields, onSuccess, onError, options);
+}
+
+function onSuccess(contacts) {
+    var ul = document.getElementById("contacts");
+    for (var i = 0; i < contacts.length; i++) {
+        var newLI = document.createElement("li");
+        newLI.innerHTML = contacts[i].name.formatted;
+        ul.appendChild(newLI);
+    }
+}
+
+function onError(){
+    alert("Failed to get contacts");
+}
 
 app.initialize();
--- www/js/index.js
+++ www/js/index.js
@@ -1,39 +1,38 @@
-function barcodescanner(){
-     cordova.plugins.barcodeScanner.scan(
-      function (result) {
-          if(result.text == 9780684852867){
-             localStorage.setItem('img', 'http://covers.openlibrary.org/b/isbn/9780684852867-L.jpg');
-             localStorage.setItem('Title', 'FIRST, BREAK ALL THE RULES');
-             location.href ="result.html";
-          }else{
-           
-          alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-              }
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      },
-      {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : false, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: false, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-          //orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS
-      }
-   );
+function barcodescanner() {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if (result.text == 9780684852867) {
+                localStorage.setItem('img', 'http://covers.openlibrary.org/b/isbn/9780684852867-L.jpg');
+                localStorage.setItem('Title', 'FIRST, BREAK ALL THE RULES');
+                location.href = "result.html";
+            } else {
+
+                alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+            }
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }, {
+            preferFrontCamera: false, // iOS and Android
+            showFlipCameraButton: false, // iOS and Android
+            showTorchButton: true, // iOS and Android
+            torchOn: false, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt: "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            //formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            //orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations: true, // iOS
+            disableSuccessBeep: false // iOS
+        }
+    );
 }
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
@@ -41,12 +40,12 @@ var app = {
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         this.receivedEvent('deviceready');
     },
 
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -55,7 +54,29 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+        listContacts();
     }
 };
+
+function listContacts() {
+    var options = new ContactFindOptions();
+    options.filter = "";
+    options.multiple = true;
+    var fields = ["*"];
+    navigator.contacts.find(fields, onSuccess, onError, options);
+}
+
+function onSuccess(contacts) {
+    var ul = document.getElementById("contacts");
+    for (var i = 0; i < contacts.length; i++) {
+        var newLI = document.createElement("li");
+        newLI.innerHTML = contacts[i].name.formatted;
+        ul.appendChild(newLI);
+    }
+}
+
+function onError(){
+    alert("Failed to get contacts");
+}
 
 app.initialize();

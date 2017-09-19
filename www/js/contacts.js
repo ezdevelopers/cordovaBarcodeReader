b81c78daa4772d4823e6f75b5d9addcfa0a393c0
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-09-19 09:45:40 +0100

    Add asynchronous contact load to boost speed

---
+++ www/js/contacts.js
@@ -0,0 +1,26 @@
+ document.addEventListener("deviceready", onDeviceReady, false);
+
+ function onDeviceReady() {
+     var options = new ContactFindOptions();
+     options.filter = "";
+     options.multiple = true;
+     //options.desiredFields = [navigator.contacts.fieldType.id];
+     options.hasPhoneNumber = true;
+     var fields = ["*"];
+     navigator.contacts.find(fields, onSuccess, onError, options);
+ };
+
+ function onSuccess(contacts) {
+     var ul = document.createElement("ul");
+     for (var i = 0; i < contacts.length; i++) {
+         var newLi = document.createElement("li");
+         newLi.innerHTML = contacts[i].name.formatted;
+         ul.appendChild(newLi);
+     }
+     localStorage.setItem("contacts", ul);
+     alert("Contacts are ready!");
+ };
+
+ function onError() {
+     alert("Failed to get contacts");
+ };

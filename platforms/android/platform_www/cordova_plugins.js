5eb97d7d72170784052db1c1ac1b239ddacd56cf
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-09-22 11:30:10 +0100

    Add native app prompt features

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -84,6 +84,22 @@ module.exports = [
         "merges": [
             ""
         ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification_android",
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -92,7 +108,8 @@ module.exports.metadata =
     "cordova-plugin-whitelist": "1.3.2",
     "cordova-plugin-compat": "1.1.0",
     "phonegap-plugin-barcodescanner": "6.0.8",
-    "cordova-plugin-contacts": "2.3.1"
+    "cordova-plugin-contacts": "2.3.1",
+    "cordova-plugin-dialogs": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
--- www/js/contacts.js
+++ www/js/contacts.js
@@ -14,8 +14,15 @@
      var ul = document.createElement("ul");
      for (var i = 0; i < contacts.length; i++) {
          var newLi = document.createElement("li");
-         var contactItem = contacts[i].name.formatted;
-         newLi.innerHTML = contactItem ;
+
+         var phoneNumbers = " ";
+         for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+            phoneNumbers += contacts[i].phoneNumbers[j].value + "\n";
+         }
+         var contactPhoneNumber = phoneNumbers;
+         var contactName = contacts[i].name.formatted;
+         newLi.innerHTML = contactName ;
+         
          ul.appendChild(newLi);
      }
      localStorage.setItem("contacts", JSON.stringify(ul));

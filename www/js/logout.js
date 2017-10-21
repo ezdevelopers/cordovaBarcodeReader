dad2c690696ed3bfbfa47b51133dc034ba97f8df
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-10-21 08:19:31 +0100

    Fix logout Feature

--- www/js/logout.js
+++ www/js/logout.js
@@ -1,31 +1,3 @@
-facebookConnectPlugin.getLoginStatus(function logout() {
-        var data = {
-            "token": localStorage.getItem("token")
-        };
-        $.ajax({
-            type: "POST",
-            contentType: 'application/json; charset=UTF-8',
-            url: "http://enshelf.com/api/logout",
-            processData: true,
-            data: JSON.stringify(data),
-            dataType: "json"
-        }).done(successFn).fail(errorFn);
-        //success function
-        function successFn(result) {
-            facebookConnectPlugin.logout(function success() {
-                    localStorage.clear();
-                    location.href = "index.html";
-                },
-                function failure() {
-                    Materialize.toast('Sorry the app could not log out of your facebook account', 3000);
-                });
-        };
-        //if there is an error in the ajax request display the error
-        function errorFn(xhr, status, strErr) {
-            Materialize.toast('There seems to be an Error in connection' + strErr, 4000);
-        };
-    },
-    function () {
         var data = {
             "token": localStorage.getItem("token")
         };
@@ -46,6 +18,4 @@ facebookConnectPlugin.getLoginStatus(function logout() {
         function errorFn(xhr, status, strErr) {
             Materialize.toast('There seems to be an Error in connection' + strErr, 4000);
         };
-    }
 
-);

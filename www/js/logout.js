54eca93ab099fe1a1f67c7750fbae6218529cbc6
Emmanuel Eze <emereuwaonueze@gmail.com>
2017-10-30 09:57:07 +0100

    Modify loan UI/UX

--- www/js/logout.js
+++ www/js/logout.js
@@ -1,23 +1,24 @@
-       function logout(){
+       function logout() {
+           Materialize.toast('Logging out', 10000);
            var data = {
-            "token": localStorage.getItem("token")
-        };
-        $.ajax({
-            type: "POST",
-            contentType: 'application/json; charset=UTF-8',
-            url: "http://enshelf.com/api/logout",
-            processData: true,
-            data: JSON.stringify(data),
-            dataType: "json"
-        }).done(successFn).fail(errorFn);
-        //success function
-        function successFn() {
-            localStorage.clear();
-            location.href = "index.html";
-        };
-        //if there is an error in the ajax request display the error
-        function errorFn(xhr, status, strErr) {
-            Materialize.toast('There seems to be an Error in connection' + strErr, 4000);
-        };
+               "token": localStorage.getItem("token")
+           };
+           $.ajax({
+               type: "POST",
+               contentType: 'application/json; charset=UTF-8',
+               url: "http://enshelf.com/api/logout",
+               processData: true,
+               data: JSON.stringify(data),
+               dataType: "json"
+           }).done(successFn).fail(errorFn);
+           //success function
+           function successFn() {
+               localStorage.clear();
+               location.href = "index.html";
+           };
+           //if there is an error in the ajax request display the error
+           function errorFn(xhr, status, strErr) {
+               Materialize.toast('There seems to be an Error in connection' + strErr, 4000);
+           };
 
-    };
+       };
